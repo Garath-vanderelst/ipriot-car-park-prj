e6d151470230763a4dc5c42cfb09eb29765a801b
Garath-vanderelst <garath.vanderelst@gmail.com>
2025-10-28 19:09:17 +0800

    chore: Added constructors and attributes to Display and Sensor

--- src/display.py
+++ src/display.py
@@ -1,2 +1,11 @@
 class Display:
-    pass
+    def __init__(self, id, message, is_on):
+        self.id = id
+        self.message = message or ''
+        self.is_on = is_on or False
+
+    def __str__(self):
+        return f'Display {id}: {message}.'
+
+
+
--- src/sensor.py
+++ src/sensor.py
@@ -1,2 +1,23 @@
 class Sensor:
+    def __init__(self, id, is_active, car_park):
+        self.id = id
+        self.is_active = is_active or False
+        self.car_park = car_park
+
+    def __str__(self):
+        if self.is_active == True:
+            sensor_status = 'Active'
+        else:
+            sensor_status = 'Inactive'
+
+        return f'{self.usage()}Sensor{self.id} at {self.car_park} is {sensor_status}'
+
+class EntrySensor(Sensor):
+    def usage(self):
+        return 'Entry'
+
+class ExitSensor(Sensor):
+    def usage(self):
+        return 'Exit'
     pass
+
