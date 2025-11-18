f9e744c194f4792dbcbf990eb888cf9e6ae9fb64
Garath-vanderelst <garath.vanderelst@gmail.com>
2025-11-18 20:24:57 +0800

    Chore: Adds unit tests for display

--- src/display.py
+++ src/display.py
@@ -1,14 +1,15 @@
 class Display:
-    def __init__(self, id, message, is_on):
+    def __init__(self, id, message, is_on, car_park):
         self.id = id
-        self.message = message or ''
         self.is_on = is_on or False
+        self.message = message
+        self.data = {'message': self.message or ''}
 
     def __str__(self):
         return f'Display {id}: {message}.'
 
     def update(self, data):
         for key, value in data.items():
-            print(f'{key}: {value}')
+            self.data[key] = value
 
 
